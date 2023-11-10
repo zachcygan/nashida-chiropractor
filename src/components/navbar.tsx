b2724d8596779a57c2fac4239913d1cd397f7937
zachcygan <zachcygan@gmail.com>
2023-11-10 13:03:25 -0800

    fixed mobile nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,6 @@ import { Fragment, useState, useEffect } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { usePathname } from 'next/navigation'
-import { AnimatePresence, motion } from "framer-motion"
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -48,127 +47,34 @@ export default function Example() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <Image
-                    src="/assets/images/logo.png"
-                    width={250}
-                    height={250}
-                    className="h-8 w-auto"
-                    alt="Your Company"
-                  />
+                  <Link
+                    key={'Home'}
+                    href={'/'}
+                  >
+                    <Image
+                      src="/assets/images/logo.png"
+                      width={250}
+                      height={250}
+                      className="h-8 w-auto"
+                      alt="Your Company"
+                    />
+                  </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8 items-center">
-                {navigation.map((item) => {
-                      const isActive = item.href === pathname;
-                      return (
-                        <Link
-                          key={item.name}
-                          href={item.href}
-                          className={`py-2 rounded-md text-md lg:text-lg relative no-underline transition-all delay-150 hover:delay-0 ease-in-out ${isActive ? "text-black hover:text-zinc-300" : "text-gray-700 dark:text-dark"}`}
-                          aria-current={item.href === pathname ? 'page' : undefined}
-                          // onClick={(e) => {
-                          //   if (item.name === 'Contact') {
-                          //     e.preventDefault(); // prevent navigation
-                          //     setIsSlideOpen(true);
-                          //   }
-                          // }}
-                          // onMouseEnter={() => {
-                          //   if (timeoutRef.current) {
-                          //     window.clearTimeout(timeoutRef.current)
-                          //   }
-                          //   setHoveredPath(item.href)
-                          // }}
-                          // onMouseLeave={() => {
-                          //   timeoutRef.current = window.setTimeout(() => {
-                          //     setHoveredPath(null)
-                          //   }, 200)
-                          // }}
-                        >
-                          {/* <AnimatePresence>
-                            {item.href === hoveredPath && (
-                              <motion.span
-                                className="absolute inset-0 rounded-lg bg-gray-100/50"
-                                layoutId="hoverBackground"
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1, transition: { duration: 0.15 } }}
-                                exit={{
-                                  opacity: 0,
-                                  transition: { duration: 0.15 },
-                                }}
-                              />
-                            )}
-                          </AnimatePresence> */}
-                          <span>{item.name}</span>
-                        </Link>
-                      );
-                    })}
+                  {navigation.map((item) => {
+                    const isActive = item.href === pathname;
+                    return (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={`py-2 rounded-md text-md lg:text-lg relative no-underline transition-all delay-150 hover:delay-0 ease-in-out ${isActive ? "text-black hover:text-zinc-300" : "text-gray-700 dark:text-dark"}`}
+                        aria-current={item.href === pathname ? 'page' : undefined}
+                      >
+                        <span>{item.name}</span>
+                      </Link>
+                    );
+                  })}
                 </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
               </div>
             </div>
           </div>
@@ -176,34 +82,19 @@ export default function Example() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pb-4 pt-2">
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-              >
-                Dashboard
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Team
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Projects
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Calendar
-              </Disclosure.Button>
+              {navigation.map((item) => {
+                const isActive = item.href === pathname;
+                return (
+                  <Disclosure.Button
+                    as="a"
+                    href={item.href}
+                    className={`block border-l-4 py-2 pl-3 pr-4 text-base font-medium ${isActive ? "border-indigo-500 bg-indigo-50 text-indigo-700" : "border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"}`}
+                    aria-current={item.href === pathname ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                );
+              })}
             </div>
           </Disclosure.Panel>
         </>

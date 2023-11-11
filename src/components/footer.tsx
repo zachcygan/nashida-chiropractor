ea7dd29ee6b202ae6852a640a7bda9fac277d179
zachcygan <zachcygan@gmail.com>
2023-11-11 13:59:28 -0800

    edited footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,7 +12,7 @@ const navigation = {
 export default function Footer() {
   return (
     <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-10 lg:px-8">
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-6">
@@ -23,7 +23,7 @@ export default function Footer() {
           ))}
         </nav>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2020 Your Company, Inc. All rights reserved.
+          &copy; Nishida Chiropractic. All rights reserved.
         </p>
       </div>
     </footer>
